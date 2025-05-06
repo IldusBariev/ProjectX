92700592453d9dba769eac84ee65ab29b09fee2d
IldusBariev <ildusbariev2006.18@gmail.com>
2025-05-06 13:03:34 +0300

    Update filesystem.c

--- filesystem.c
+++ filesystem.c
@@ -1,5 +1,29 @@
 #include <stdio.h>
 
+void work_with_file(const char* file, const char* action) {
+    printf("!");
+}
+
+char* read_file(const char* file) {
+    FILE *fp;
+    fp = fopen(file, "r");
+    
+    if (fp == NULL) {
+        perror("Ошибка. Файл не существует либо не правильно написан");
+        return "Error";
+    }
+    
+    int length;
+    printf("Введите длину строки, которую вы хотите\n");
+    scanf("%d", &length);
+    
+    char line[length];
+    fgets(line, sizeof(line), fp);
+    fclose(fp);
+    
+    return line;
+}
+
 void fs_delete(const char* file) {
     if (remove(file) == 0) printf("Файл удален\n");
     else perror("Ошибка. Файл не существует либо не правильно написан");
