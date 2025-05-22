fa35fc8d43ee94cf51d4609e066abd31e3742ca7
IldusBariev <ildusbariev2006.18@gmail.com>
2025-05-22 11:23:52 +0300

    Update filesystem.h

--- filesystem.h
+++ filesystem.h
@@ -1,7 +1,5 @@
-void file_delete(const char* file);
-
-char* append_file (const char* file);
-char* read_file(const char* file);
-char* update_file( const char* file);
-char* read_and_work(const char* file);
-char* read_and_append(const char* file);
+FILE* create_open_file(const char* filename);
+char* view_file(const char* filename);
+char* delete_file(const char* filename);
+char* add_file(const char* main_filename);
+char* modify_file(const char* filename);
